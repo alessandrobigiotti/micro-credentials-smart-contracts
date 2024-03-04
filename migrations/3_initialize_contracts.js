0f94206038849da66da3e730a1359193087c9651
alessandrobigiotti <alessandro.bigiotti@gmail.com>
2024-03-04 18:25:06 +0000

    update initialize_contracts

--- migrations/3_initialize_contracts.js
+++ migrations/3_initialize_contracts.js
@@ -1,15 +1,6 @@
 const path = require('path');
 const fs = require('fs');
 
-const Initiator = artifacts.require("Initiator");
-const Finalizer = artifacts.require("Finalizer");
-const DataStorage = artifacts.require("DataStorage");
-const SimpleToken = artifacts.require("SimpleERC20Token");
-const SimpleERC721 = artifacts.require("SimpleERC721Token");
-const SamplesStorage = artifacts.require("SamplesStorage");
-const SamplesCalculateAverage = artifacts.require("SamplesCalculateAverage");
-
-
 module.exports = async function (deployer, network) {
     console.log("Nothing to initialise...");
 }
